a745737958a7a860b6189aa71ab110086936f97a
manyuan0525 <manyuan@bu.edu>
2019-05-27 08:59:53 +0800

    Update LongestSubstring.java

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -1,18 +1,27 @@
 public class Solution {
+    // problem: return the longestsubstring without repeating in the given string
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        Set<Character> set = new HashSet<>();
-        int ans = 0, i = 0, j = 0;
-        while (i < n && j < n) {
-            // try to extend the range [i, j]
-            if (!set.contains(s.charAt(j))){
-                set.add(s.charAt(j++));
-                ans = Math.max(ans, j - i);
-            }
-            else {
-                set.remove(s.charAt(i++));
+        // Build a new list with length 128(# of ASCII code)
+        int[] characters = new int[128];
+        int left = 0;
+        int max = 0;
+        // loop the string
+        for (int i = 0; i < s.length(); i++) {
+            // get the char of corresponding index
+            char c = s.charAt(i);
+            // there exists repetition
+            if (characters[c] > 0) {
+                // compute the max substring so far
+                max = Math.max(max, i - left);
+                // if there are two same char, restart left at i 
+                if (s.charAt(i - 1) == c) left = i;
+                else left = Math.max(left, characters[c]);
+                characters[c] = i + 1;
+            } else {
+                characters[c] = i + 1; 
             }
         }
-        return ans;
+        max = Math.max(max, s.length() - left);
+        return max == 0 ? s.length() : max;
     }
 }
